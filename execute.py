202c1ad66b04dd05657cc857d5730afe4d498fd4
Arnav Patel <patelarnav96@gmail.com>
2017-09-15 15:30:18 -0400

    update execute.py and fix typo in README

--- execute.py
+++ execute.py
@@ -3,8 +3,26 @@ from nytimesarchive import ArchiveAPI
 api = ArchiveAPI('3d07f455158e48a7867a80541a58435b')
 
 def getResults():
-    year = input("Please type a year: ")
-    month = input("Please type a month: ")
-    print(api.query(year, month))
+    year = int(input("Please type a year: "))
+    month = int(input("Please type a month: "))
+    x = api.query(year, month)
+    return x['response']['docs']
 
-getResults()
+def pullRelevantInfo(docs):
+    l = []
+    for article in docs:
+        updated_article={'web_url':article['web_url'],
+                  'pub_date':article['pub_date'],
+                  'headline':article['headline'],
+                  'snippet':article['snippet']
+                  }
+        if article['section_name']:
+            updated_article['section']= article['section_name']
+        l.append(updated_article)
+        
+    return l
+
+
+x = pullRelevantInfo(getResults())
+
+

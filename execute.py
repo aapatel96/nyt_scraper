605f9f4b9753ec34882ed2de8f431a72811a5486
Arnav Patel <patelarnav96@gmail.com>
2017-09-28 10:42:09 -0400

    add loop through years && update getResults to take year and month as param

--- execute.py
+++ execute.py
@@ -1,5 +1,4 @@
 from nytimesarchive import ArchiveAPI
-import os
 import pymongo
 
 client = pymongo.MongoClient('mongodb://heroku_d0df7lpt:i4k9nglftvest2g14p69tkrbph@ds135534.mlab.com:35534/heroku_d0df7lpt')
@@ -9,9 +8,7 @@ articles = db['articles']
 
 api = ArchiveAPI('3d07f455158e48a7867a80541a58435b')
 
-def getResults():
-    year = int(input("Please type a year: "))
-    month = int(input("Please type a month: "))
+def getResults(year, month):
     x = api.query(year, month)
     return x['response']['docs']
 
@@ -23,13 +20,14 @@ def pullRelevantInfo(docs):
                   'headline':article['headline'],
                   'snippet':article['snippet']
                   }
-        try:
+        if article['section_name']:
             updated_article['section']= article['section_name']
-        except:
-            pass
-        articles.insert_one(updated_article)
+        ##articles.insert_one(updated_article) this is commented out because I do not want to risk double adding the articles in the db
         l.append(updated_article)
+
     return l
 
-
-x = pullRelevantInfo(getResults())
+newslists =[]
+for year in range(1997,2018):
+  articles = pullRelevantInfo(getResults(year,11))
+  newslists.append(articles)
